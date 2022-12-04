46e5836ede5785e9435d0531970df5425200cd72
akshay <ak5967751@gmail.com>
2022-12-04 22:21:37 +0530

    fourth commit

--- holidatebackend/src/database/connect.js
+++ holidatebackend/src/database/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/holidateusers").then(()=>{
+mongoose.connect("mongodb+srv://holidateuser:password@drowssap@cluster0.nsua80r.mongodb.net/holidateusers?retryWrites=true&w=majority").then(()=>{
     console.log("connection established");
 }).catch((err)=>{
     console.log(err);
